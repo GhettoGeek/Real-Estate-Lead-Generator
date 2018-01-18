cd83f5fee5fe5d219968f4fc9b8e8ef36251a04e
sergio <scrummish@gmail.com>
2018-01-18 14:45:54 -0600

    Dummy Forms

--- controllers/userController.js
+++ controllers/userController.js
@@ -7,4 +7,20 @@ router.route('/')
 		res.send('works')
 	})
 
+router.route('/login')
+	.get((req,res)=>{
+		res.render('login.ejs')
+	})
+	.post((req,res)=>{
+		res.send(req.body);
+	})
+
+router.route('/register')
+	.get((req,res)=>{
+		res.render('register.ejs')
+	})
+	.post((req,res)=>{
+		//add user to db and redirect to home
+	})
+
 module.exports  = router;
