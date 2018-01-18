fa5e69a762c70634e7675cb2a20b979fa98c329d
sergio <scrummish@gmail.com>
2018-01-18 15:01:16 -0600

    Registration post route adds data to db

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/user.js');
+const bcrypt = require('bcrypt');
 
 router.route('/')
 	.get((req,res)=>{
@@ -21,6 +22,21 @@ router.route('/register')
 	})
 	.post((req,res)=>{
 		//add user to db and redirect to home
+		const password = req.body.password;
+		const hashword = bcrypt.hashSync(password, bcrypt.genSaltSync(10));
+
+		const newDbEntry = {
+			username: req.body.username,
+			password: hashword
+		}
+
+		User.create(newDbEntry, (err,created)=>{
+			if (err){
+				res.send(err)
+			} else {
+				res.send(created);
+			}
+		})
 	})
 
 module.exports  = router;
