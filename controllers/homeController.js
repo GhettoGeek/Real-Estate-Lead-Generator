34a49cab4355bc05f7f77a8f66063e40d876a6d4
sergio <scrummish@gmail.com>
2018-01-22 14:15:24 -0600

    App now shows query of houses

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -8,17 +8,23 @@ router.route('/')
 		res.render('home.ejs',{signedIn: req.session.loggedIn})
 	})
 	.post((req,res)=>{
+		// Finds all the homes that match the users options
 		Homes.find({$and:[{neighborhood: req.body.neighborhood},{bedrooms: { $gte: req.body.min }},{bedrooms: { $lte: req.body.max }}]},(err,foundHomes) => {
-			console.log(foundHomes);
-		});
+			// Render a page showing all the found homes for the user
+			res.render('showmany.ejs',{
+				homes: foundHomes,
+				signedIn: req.session.loggedIn
+			})
+		})
 	})
 
 router.route('/:id')
 	.get((req,res)=>{
-		res.render('home.ejs',{
-			page: req.params.id,
-			username: req.session.username
-		})
+		// res.render('home.ejs',{
+		// 	page: req.params.id,
+		// 	username: req.session.username
+		// })
+		res.send('working')
 	})
 
 
--- models/user.js
+++ models/user.js
@@ -1,11 +1,12 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.schema;
+const Homes = require('./homes.js');
 
 const userSchema = new mongoose.Schema({
 	email: {type: String, required: true, unique: true},
 	password: {type: String, required: true},
 	fullname: {type: String, required: true},
-	requestedProperties: [],
+	requestedProperties: [Homes.schema]
 })
 
 module.exports = mongoose.model('User', userSchema);
