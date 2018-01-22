e082d2b85a93457a6de83b652a58be1417ec18d7
Travis1282 <tmail20@gmail.com>
2018-01-22 15:15:59 -0600

    Our agents page listing agents

--- models/userModel.js
+++ models/userModel.js
@@ -1,13 +1,14 @@
 const 	mongoose = require('mongoose'),
 		Schema = mongoose.schema,
-		Agent = require('./agentModel.js')
+		Agent = require('./agentModel.js'), 
+		Homes = require('./homes.js');
 
 const userSchema = new mongoose.Schema({
 	email: {type: String, required: true, unique: true},
 	password: {type: String, required: true},
 	fullname: {type: String, required: true},
 	idArray: [String],
-	requestedProperties: [{}],
+	requestedProperties: [Homes.schema],
 	agent: [Agent.schema]
 
 })
