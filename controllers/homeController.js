61f781d02c908b163228c290553b6b7c1710e5b7
sergio <scrummish@gmail.com>
2018-01-22 22:36:40 -0600

    Request a showing button now creates the request ans stores it both in the users list of requests and in the request collection

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,7 +1,8 @@
-const	express = require('express'),
-		router = express.Router(),
-		User = require('../models/userModel.js');
+const express = require('express');
+const router = express.Router();
+const User = require('../models/userModel.js');
 const Homes = require('../models/homes.js');
+const Request = require('../models/requestModel.js');
 
 router.route('/')
 	.get((req,res)=>{
@@ -20,27 +21,31 @@ router.route('/')
 	
 router.route('/:id')
 	.get((req,res)=>{
-		// console.log(req.params.id)
 		Homes.findById(req.params.id,(err,foundHome)=>{
 			res.render('show.ejs',{
 				home: foundHome,
 				signedIn: req.session.loggedIn
-			// username: req.session.username
 			})
 		})
 	})
 	.post((req,res)=>{
-		// const request = {
-		// 	propertyId: req.params.id,
-		// 	userId: String,
-		// 	created: Date,
-		// 	accepted: false,
-		// 	completed: false
-		// }
-		console.log(req.session.email);
+		// Find the user who requested the showing
+		User.findOne({email: req.session.email}, (err,found)=>{
+			// Create a request to view the home
+			const showingRequest = {
+				propertyId: req.params.id,
+				userId: found.id,
+				created: new Date(),
+				accepted: false,
+				completed: false
+			}
+			Request.create(showingRequest,(err,created)=>{
+				// Push the created request into the users list of requests
+				found.requestedProperties.push(created);
+				// Save the changed user
+				found.save((err)=>{if (err) return handleError(err)});
+			})
+		})
 	})
-
-
-
 
 module.exports = router;
